ec95f66a80a0c8e5dafff6c9e55699b8f869f54c
Tommy Miyazaki <tommyzki@toms-macbook-pro.local>
2017-03-08 11:07:34 +0700

    Initial Commit

---
+++ TestGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by Tommy Miyazaki on 3/8/17.
+//  Copyright © 2017 Tom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
