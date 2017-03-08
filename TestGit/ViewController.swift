b562426f2dcf3ba3397db925ba3e0c55e6f21878
Tommy Miyazaki <tommyzki@Toms-MacBook-Pro.local>
2017-03-08 15:21:56 +0700

    no message

--- TestGit/ViewController.swift
+++ TestGit/ViewController.swift
@@ -9,10 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var weatherLabel: UILabel!
+    
+    var weather = DataModel()
+    
+    func updateUI() {
+        dateLabel.text = weather.date
+        tempLabel.text = "\(weather.temp)"
+        locationLabel.text = weather.location
+        weatherLabel.text = weather.weather
+        weatherImage.image = UIImage(named: weather.weather)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        weather.downloadData {
+            self.updateUI()
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
